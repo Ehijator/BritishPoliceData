a11afa8b3d44b8884c50e7f729cd3d4ac0a45ff4
Ehijator <ehijatora@gmail.com>
2022-08-07 17:31:49 +0100

    Dimensional modelling scripts

---
+++ DimLocation_TableCreation.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS DimLocation
+CREATE TABLE DimLocation
+(
+[location.street.id] [int] NOT NULL PRIMARY KEY identity,
+[Location_type] [char](20),
+[location_subtype] [char](20),
+[location.street.name] [char](60),
+[location.longitude] [decimal](9,6), 
+[location.latitude] [decimal](8,6),
+)
