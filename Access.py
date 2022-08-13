d48df7ffeea49b056da447c72d7f3303b6da7273
Ehijator <ehijatora@gmail.com>
2022-08-13 17:14:09 +0100

    Add Historical data package and changed file structure

--- Access.py
+++ Access.py
@@ -11,7 +11,8 @@ class Access:
         self.session = requests.Session()
         self.err = "404: Data requested not found"
         self.start_year="2020"
-        
+
+
 
     def historic_Pipeline(self): #used for collecting historical
         datestart = ddate(2019,6,1)
@@ -34,18 +35,20 @@ class Access:
         
 
 
-
     def monthly_Pipeline(self): #this will be run daily to check if the api has been updated 
+        df = pd.DataFrame()
         currentdate = ddate.today().strftime("%Y-%m")
         response = requests.get(self.baseurl+str(currentdate))
         if response.status_code == 200:
-            pd.json_normalize(response.json())
+            batch = pd.json_normalize(response.json())
+            df = df.append(batch)
+            df.to_csv("raw_police.csv")
+            print("saved csv")
         else: 
            pass
 
     
     
-
     def load_To_Db(self): #ingests data to SQL server
         df = pd.read_csv(r'C:\Python310\Scripts\PoliceAPI\raw_police.csv')
         df= df.iloc[:,1:] #dropping index column from df       
@@ -55,17 +58,23 @@ class Access:
 
         df.to_sql('POLICEAPI_STAGING',engine,if_exists='append',index=False)
 
-    def main(self):
+
+
+    def historicalMain(self): #used in the case of data loss, reloads all data from api
         self.historic_Pipeline()
         self.load_To_Db()
-        
         print ("Data ingested successfully")
-        
+
+
+
+    def dailyMain(self):
+        self.monthly_Pipeline()
+        self.load_To_Db()        
+        print ("Data ingested successfully")
 
         
 a = Access()
-a.main()
-Access().main()
+a.historicalMain()
 
         
 
